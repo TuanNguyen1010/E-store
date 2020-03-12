00c75bc04bdbf31490a0e712a54949a4576ea530
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-12 18:26:44 +0000

    Fix: fixed repeat email bug

--- models/user.js
+++ models/user.js
@@ -9,13 +9,13 @@ const userSchema = new mongoose.Schema({
     required: true, 
     maxlength: 32,
 
-  }, 
+  },
   email: {
-    type: String, 
-    trim: true,
-    required: true, 
-    maxlength: 70
-  }, 
+      type: String,
+      trim: true,
+      required: true,
+      unique: true
+  },
   hashed_password: {
     type: String, 
     required: true
