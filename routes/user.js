b514d540899b8d73f46e08f128bfa698f150ee57
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-13 16:23:00 +0000

    Chore: User validator connected to routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router()
+const {userSignupValidator} = require('../validator')
 
 const {signup} = require('../controllers/user')
 
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, signup);
 
 
 module.exports = router;
--- validator/index.js
+++ validator/index.js
@@ -1,13 +1,13 @@
 exports.userSignupValidator = (req, res, next) => {
-  req.check('name', 'Name is required').noEmpty()
+  req.check('name', 'Name is required').notEmpty()
   req.check('email', 'Email must be between 3 and 32 characters')
-      .match(/.+\@.+\.,+/)
+      .matches(/.+\@.+\..+/)
       .withMessage('email must contain @')
       .isLength({
         min:4, 
         max:32
       });
-      req.check('password', 'Password is required').noEmpty()
+      req.check('password', 'Password is required').notEmpty()
       req.check('password')
       .isLength({min:6})
       .withMessage('Password must contain 6 characters')
@@ -15,7 +15,7 @@ exports.userSignupValidator = (req, res, next) => {
       .withMessage('Password must contain a number')
         const errors = req.validationErrors()
         if(errors) {
-          const firstError = errors.map(error => error.message) [0]
+          const firstError = errors.map(error => error.msg) [0]
           return res.status(400).json({ error: firstError})
         }
         next()
