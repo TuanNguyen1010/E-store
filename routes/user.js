7950acedd0edd1b57303ce16eb2c5844de32dea9
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-16 21:46:37 +0000

    Fix: sign in functionality fixed

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router()
+const {signup, signin} = require('../controllers/user')
 const {userSignupValidator} = require('../validator')
 
 
 router.post('/signup', userSignupValidator, signup);
-router.post('./signin', signin)
+router.post('/signin', signin);
 
 
 module.exports = router;
