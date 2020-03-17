915b08ad57f5ede93f9c531510a29967eb27f0e3
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-17 15:53:14 +0000

    Test: tested signup functionality

--- test/unit/authourisation.test.js
+++ test/unit/authourisation.test.js
@@ -1,10 +1,24 @@
 const AuthourisationController = require('../../controllers/authourisation')
+const UserModel = require('../../models/user')
+const httpMocks = require('node-mocks-http')
+const signUpData = require('../mock-data/signUpData')
+
+
+UserModel.save = jest.fn();
 
 describe('AuthourisationController.signup', () => {
   it('should have a signup function', () => {
     expect(typeof AuthourisationController.signup).toBe("function")
   })
-
+  it('should call User.create on models', () => {
+    let req, res;
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+    req.body = signUpData;
+    AuthourisationController.signup(req, res);
+    expect(UserModel.save).toBeCalledWith(signUpData);
+  })
 })
 
 describe('AuthourisationController.signin', () => {
